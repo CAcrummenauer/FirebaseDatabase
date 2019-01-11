dc767f2301bcc83eed953647b2d0d2e28f82a6cb
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-11 21:55:10 -0200

    Tratando o evento child_added em tempo real...

--- database.js
+++ database.js
@@ -1,10 +1,18 @@
 preObject = document.getElementById('preObject');
-
+list = document.getElementById('list');
 // Referenciando 'object' no bando de dados:
 const db = firebase.database().ref().child('object');
+const dbList = db.child('Cursos');
 
 // Escutando qualquer tipo de mudança em 'object':
 db.on('value', snap => {
-   console.log(snap.val());
-   preObject.innerText = JSON.stringify(snap.val(), null, 3)
+   // console.log(snap.val());
+   // preObject.innerText = JSON.stringify(snap.val(), null, 3)
+});
+
+dbList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerHTML = snap.val();
+    li.id = snap.key;
+    list.appendChild(li);
 });
